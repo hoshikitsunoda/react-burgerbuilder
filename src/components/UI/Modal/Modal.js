398185b46b40d07a592b1423f1ae6decb956a4c5
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-02-05 20:33:42 -0800

    Spelling in styling fixed. #7

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,7 +6,7 @@ const modal = (props) => (
   <div
     className={classes.Modal}
     style={{
-      transform: props.show ? 'traslateY(0)' : 'translateY(-100vh)',
+      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
       opacity: props.show ? '1' : '0'
     }}>
     {props.children}
