3485cba5404defa71d5691622f97de31f45cf4a5
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-02-05 21:10:28 -0800

    Added clicked method in Backdrop component. #7

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,7 +6,7 @@ import Aux from '../../../hoc/Aux'
 
 const modal = (props) => (
   <Aux>
-    <Backdrop show={props.show} />
+    <Backdrop show={props.show} clicked={props.modalClosed} />
     <div
       className={classes.Modal}
       style={{
