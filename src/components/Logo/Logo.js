caf74fd049c3981795a1803175c7e9370531eb7c
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-02-06 18:38:39 -0800

    Logo image imported to Logo.js. #9

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import burgerLogo from '../../assets/Images/burger-logo.png'
+
+const logo = (props) => (
+  <div>
+    <img src={burgerLogo} alt='MyBurger' />
+  </div>
+)
+
+export default logo
