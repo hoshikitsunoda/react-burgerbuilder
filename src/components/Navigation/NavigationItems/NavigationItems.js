219b0e1975087edd6a530b4e40fbe346cd8c771d
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-02-06 18:59:53 -0800

    Imported NavigationItem. #9

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 import classes from './NavigationItems.css'
+import NavigationItem from './NavigationItem'
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-
+    <NavigationItem />
   </ul>
 )
 
