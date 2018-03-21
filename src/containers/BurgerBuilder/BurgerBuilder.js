f192b4a531c6c44f0fe2561bfcee6d4f6575143f
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-03-20 21:52:09 -0700

    Add navigation to Checkout page #15

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,7 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount () {
+    console.log(this.props);
     axios.get('https://react-burgerbuilder-f43c0.firebaseio.com/ingredients.json')
       .then(response => {
         this.setState({ingredients: response.data})
@@ -88,28 +89,29 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!')
-    this.setState({loading: true})
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Hoshiki Tsunoda',
-        address: {
-          street: 'Canyon Dr',
-          zipCode: '92626',
-          country: 'USA'
-        },
-        email: 'shiru@shiru.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({loading: false, purchasing: false})
-      })
-      .catch(error => {
-        this.setState({loading: false, purchasing: false})
-      })
+    // this.setState({loading: true})
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Hoshiki Tsunoda',
+    //     address: {
+    //       street: 'Canyon Dr',
+    //       zipCode: '92626',
+    //       country: 'USA'
+    //     },
+    //     email: 'shiru@shiru.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({loading: false, purchasing: false})
+    //   })
+    //   .catch(error => {
+    //     this.setState({loading: false, purchasing: false})
+    //   })
+    this.props.history.push('/checkout')
   }
 
   render() {
