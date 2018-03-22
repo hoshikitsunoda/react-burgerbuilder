7e6c8a5e7e6adfa4fde7f5745d9be1a1c6c4abba
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-03-21 20:13:39 -0700

    Add order submission and pass data between pages #15

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,32 +89,11 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!')
-    // this.setState({loading: true})
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Hoshiki Tsunoda',
-    //     address: {
-    //       street: 'Canyon Dr',
-    //       zipCode: '92626',
-    //       country: 'USA'
-    //     },
-    //     email: 'shiru@shiru.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // }
-    // axios.post('/orders.json', order)
-    //   .then(response => {
-    //     this.setState({loading: false, purchasing: false})
-    //   })
-    //   .catch(error => {
-    //     this.setState({loading: false, purchasing: false})
-    //   })
     const queryParams = []
     for(let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
     }
+    queryParams.push('price=' + this.state.totalPrice)
     const queryString = queryParams.join('&')
     this.props.history.push({
       pathname: '/checkout',
